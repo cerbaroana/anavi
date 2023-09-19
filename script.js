fe8bfe759a18d9f86f33ae531bfead9239970143
cerbaroana <137786775+cerbaroana@users.noreply.github.com>
2023-09-19 11:47:00 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+calcule.addEventListener("click", function () {
+  var a = valorA.value;
+  var b = valorb.value;
+  var op = operacao.value;
+
+  a = parseInt(a);
+  b = parseInt(b);
+
+  alert(a + b);
+});
